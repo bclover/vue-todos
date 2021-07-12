be91b2a74151489855c7fbf04c815a5febda5479
Bryan Clover <bclover09@gmail.com>
2021-07-12 13:27:32 -0400

    show new task to add in right column

--- src/components/RightColumn.vue
+++ src/components/RightColumn.vue
@@ -1,41 +1,55 @@
 <template>
   <v-container>
 
-    <v-row class="ma-5 pa-3 right-col" no-gutters>
-      <v-col class="ma-0 pa-0" cols="12">
-      <!-- ADD A TO DO -->
-      <v-btn
-        :disabled="todoExists"
-        depressed
-        color="secondary"
-        @click="addTodo"
-      >
-        {{ btnAdd }}
-      </v-btn>
+    <v-row class="ma-5 pa-3 right-col flex-column" no-gutters>
+      <v-col class="ma-0 mb-5 pa-0" cols="12">
 
-      <!-- CLEAR ALL TO DOS -->
-      <v-btn
-        :disabled="totalNumTodos === 0"
-        depressed
-        class="ml-3"
-        color="secondary"
-        @click="clearAllTodos"
-      >
-        {{ btnClear }}
-      </v-btn>
+        <!-- CLEAR ALL TO DOS -->
+        <v-btn
+          :disabled="totalNumTodos === 0"
+          depressed
+          color="secondary"
+          @click="clearAllTodos"
+        >
+          {{ btnClear }}
+        </v-btn>
 
-      <!-- RELOAD ALL TO DOS -->
-      <v-btn
-        :disabled="totalNumTodos > 0"
-        depressed
-        class="ml-3"
-        color="secondary"
-        @click="reloadTodos"
-      >
-        {{ btnReload }}
-      </v-btn>
+        <!-- RELOAD ALL TO DOS -->
+        <v-btn
+          :disabled="totalNumTodos > 0"
+          depressed
+          class="ml-3"
+          color="secondary"
+          @click="reloadTodos"
+        >
+          {{ btnReload }}
+        </v-btn>
+      </v-col>
+      <v-col v-show="!todoExists" class="pt-3" cols="12">
+        <div>New Task to Add:</div>
+        <div class="pa-3 new-todo">
+          <span class="todo--title">
+            To Do #{{newTodo.id}}: "{{ newTodo.title }}"
+          </span>
+          Priority:
+          <span class="error--text">{{ newTodo.priority }}</span><br>
+          </div>
+        </div>
+        <div>
+          <!-- ADD A TO DO -->
+          <v-btn
+            :disabled="todoExists"
+            depressed
+            class="mt-3"
+            color="secondary"
+            @click="addTodo"
+          >
+            {{ btnAdd }}
+          </v-btn>
+        </div>
       </v-col>
     </v-row>
+
 
   </v-container>
 </template>
@@ -62,19 +76,19 @@ export default {
       btnAdd: 'Add',
       btnClear: 'Clear All',
       btnReload: 'Reload',
+      newTodo: {
+        id: 11,
+        title: 'Resolve CORS issues with server team.',
+        priority: 'Life Changing',
+        assigned: 'Bryan Clover',
+      },
     };
   },
   methods: {
 
     // ADD A TO DO
     async addTodo() {
-      const newTodo = {
-        id: 11,
-        title: 'Resolve CORS issues with server team.',
-        priority: 'Life Changing',
-        assigned: 'Bryan Clover',
-      };
-      await this.$store.dispatch(ADD_TODO, newTodo);
+      await this.$store.dispatch(ADD_TODO, this.newTodo);
     },
 
     // REMOVE ALL
@@ -95,5 +109,17 @@ export default {
   background-color: whitesmoke;
   border-radius: 0.5rem;
   min-height: 96vh !important;
+}
+.new-todo {
+  background-color: #c0c9dc;
+  border: 0.125rem solid #214395;
+  border-radius: 0.5rem;
+  opacity: .75;
+}
+.todo--title {
+  display: flex;
+  font-size: 1.25rem;
+  font-weight: bold;
+  justify-content: space-between;
 }
 </style>
